cdba8d467def3856c45010df7442fe5646d0bfa6
Medna Dreamheart <medozcarpaccio@gmail.com>
2022-11-08 19:23:05 +0500

    Add Initial MVC Website

---
+++ SynelEmployees/Controllers/EmployeesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SynelEmployees.Controllers;
+public class EmployeesController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
