c583a0cb9c41e98c9fac881972baa3c1fa2f2180
Medna Dreamheart <medozcarpaccio@gmail.com>
2022-11-08 19:21:21 +0500

    Add Employees Database

---
+++ Employees/dbo/StoredProcedures/spEmployee_Add.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[spEmployee_Add]
+	@PayrollNumber nvarchar(50),
+    @Forename nvarchar(50),
+    @Surname nvarchar(50),
+    @BirthDate datetime2(7),
+    @Phone nvarchar(20),
+    @CellPhone nvarchar(20),
+    @StreetAddress nvarchar(50),
+    @City nvarchar(50),
+    @Postcode nvarchar(20),
+    @Email nvarchar(50),
+    @StartDate datetime2(7)
+AS
+
+begin
+    insert into [dbo].[Employee] ([PayrollNumber], [Forename], [Surname], [BirthDate], [Phone], [CellPhone], [StreetAddress], [City], [Postcode], [Email], [StartDate])
+    values (@PayrollNumber, @Forename, @Surname, @BirthDate, @Phone, @CellPhone, @StreetAddress, @City, @Postcode, @Email, @StartDate);
+end
---
+++ Employees/dbo/Tables/Employee.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [PayrollNumber] NVARCHAR(50) NOT NULL, 
+    [Forename] NVARCHAR(50) NOT NULL, 
+    [Surname] NVARCHAR(50) NOT NULL, 
+    [BirthDate] DATETIME2 NOT NULL, 
+    [Phone] NVARCHAR(20) NOT NULL, 
+    [CellPhone] NVARCHAR(20) NOT NULL, 
+    [StreetAddress] NVARCHAR(50) NOT NULL, 
+    [City] NVARCHAR(50) NOT NULL, 
+    [Postcode] NVARCHAR(20) NOT NULL, 
+    [Email] NVARCHAR(50) NOT NULL, 
+    [StartDate] DATETIME2 NOT NULL DEFAULT getutcdate()
+)
